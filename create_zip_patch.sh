35df3e248a9f27257c0b5ba770aed011ff0b0b4d
philha <philha@worteks.com>
2022-11-22 19:34:50 +0100

    add docker-entrypoint.sh within patch
    - docker-entrypoint was missing.
    - checked working with patched by unzip.

--- create_zip_patch.sh
+++ create_zip_patch.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # tgz version
-tar -czf radius_patch.tgz freeradius patch Dockerfile RADIUS_ATTRIBUTES.md README.md
+tar -czf radius_patch.tgz freeradius patch Dockerfile RADIUS_ATTRIBUTES.md README.md docker-entrypoint.sh
 # zip version
-zip radius_patch.zip -r freeradius/ patch/ Dockerfile RADIUS_ATTRIBUTES.md README.md
+zip radius_patch.zip -r freeradius/ patch/ Dockerfile RADIUS_ATTRIBUTES.md README.md docker-entrypoint.sh
 
+sha256sum radius_patch.tgz radius_patch.zip >radius_patch.sha256
